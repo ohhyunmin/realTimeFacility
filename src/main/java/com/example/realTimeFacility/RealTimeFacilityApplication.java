c39d3019c9d90eae8a0eeaa6c5f87fb5b26893d9
NoPln <66984443+ohhyunmin@users.noreply.github.com>
2021-11-08 21:14:33 +0900

    Initial commit

---
+++ src/main/java/com/example/realTimeFacility/RealTimeFacilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.realTimeFacility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeFacilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeFacilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/realTimeFacility/RealTimeFacilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realTimeFacility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeFacilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
